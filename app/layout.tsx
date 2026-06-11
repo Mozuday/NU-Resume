969161e2737c1705d8bf03fcad5bb9e55ab9f6ad
Mozuday <uday88687@gmail.com>
2026-06-11 15:11:21 +0530

    Added Monetag script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,37 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
-
     <html lang="en">
-
       <body>
-
         {children}
 
-        <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+        {/* Razorpay */}
+        <Script
+          src="https://checkout.razorpay.com/v1/checkout.js"
+          strategy="afterInteractive"
+        />
 
+        {/* Monetag */}
+        <Script id="monetag" strategy="afterInteractive">
+          {`
+            (function(s){
+              s.dataset.zone='11131993',
+              s.src='https://nap5k.com/tag.min.js'
+            })(
+              [document.documentElement, document.body]
+                .filter(Boolean)
+                .pop()
+                .appendChild(document.createElement('script'))
+            );
+          `}
+        </Script>
       </body>
-
     </html>
-
   );
 }
