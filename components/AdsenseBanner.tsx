1f2354044a4f18fd0c8f27f8a29b1c4b9a4b37ec
Uday Sharma <uday88687@gmail.com>
2026-06-15 13:57:52 +0530

    Rename AdsterraBanner to AdsenseBanner with updates

---
+++ components/AdsenseBanner.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    adsbygoogle: any[];
+  }
+}
+
+export default function AdsenseBanner() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <div className="mt-6 flex justify-center">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", width: "100%" }}
+        data-ad-client="ca-pub-7618293831899526"
+        data-ad-slot="YOUR_AD_SLOT_ID"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
+  );
+}
